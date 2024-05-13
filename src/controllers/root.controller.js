3994b501270df42f9c2f2e5fee76bb8264e6b4c4
XV Yashar <xvyashar@gmail.com>
2024-05-13 17:53:58 +0330

    ref: replace function with arrow function

--- src/controllers/root.controller.js
+++ src/controllers/root.controller.js
@@ -1,7 +1,7 @@
 const { HTTP_STATUS } = require('../constants');
 
 module.exports = {
-  helloWorld: function (_req, res) {
+  helloWorld: (_req, res) => {
     res
       .status(HTTP_STATUS.OK)
       .json({ message: 'Hello World!', statusCode: HTTP_STATUS.OK });
